96f1f7a23d333f13d6fe7159b31cb38f727e8bbb
0pigduck0 <147368350+0pigduck0@users.noreply.github.com>
2024-11-14 12:48:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/aa/')
 def home():
     return render_template('index.html')
 
-@app.route('/static_analysis')
+@app.route('/aa/static_analysis')
 def static_analysis_page():
     return render_template('static_analysis.html')
 
-@app.route('/dynamic_analysis')
+@app.route('/aa/dynamic_analysis')
 def dynamic_analysis_page():
     return render_template('dynamic_analysis.html')  # dynamic_analysis.html 파일 로드
 
-@app.route('/api_documentation')
+@app.route('/aa/api_documentation')
 def api_documentation():
     return render_template('api_documentation.html')
 
-@app.route('/about')
+@app.route('/aa/about')
 def about():
     return render_template('about.html')
 
